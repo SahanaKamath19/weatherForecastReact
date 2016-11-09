86a2712c4792da245756b3779a266a0fca6e8058
SahanaKamath19 <sahana.nayak16@gmail.com>
2016-11-09 12:47:30 -0500

    accessing data from backend API call

--- backEnd/server.js
+++ backEnd/server.js
@@ -2,33 +2,43 @@ const express = require('express');
 const axios = require('axios');
 const request =  require('request');
 const app = express();
-let city = "Delhi";
-url = "http://api.openweathermap.org/data/2.5/forecast?q="+city+"&units=metric&APPID=517985c009ccca68942debb4afe4feb2";
-
-// app.get('/', (req,res) => {
-//     request(url, (error,response,body)=>{})
-// })
-
-request(url, function (error, response, body) {
- if (!error && response.statusCode == 200) {
-//    console.log(body);
-   var list = JSON.parse(body).list;
-   var weather = [];
-    for(let i=0;i<list.length;i++){
-        weather.push(
-            {"temp":list[i].main.temp,
-            "tempMin":list[i].main.temp_min,
-            "tempMax":list[i].main.temp_max,
-            "humidity":list[i].main.humidity,
-            "date":list[i].dt_txt,
-            "icon":list[i].weather[0].icon,
-            "description":list[i].weather[0].description
-        });
-    }
-    console.log(weather);
- }
  
+app.use(function(req, res, next) {
+ res.header("Access-Control-Allow-Origin", "*");
+ res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, authorization");
+ next();
+});
+
+let city = "Toronto";
+url = "http://api.openweathermap.org/data/2.5/forecast/daily?q="+ city +"&units=metric&APPID=517985c009ccca68942debb4afe4feb2";
+//url = "http://api.openweathermap.org/data/2.5/forecast?q="+city+"&units=metric&APPID=517985c009ccca68942debb4afe4feb2";
+
+app.get('/', (req,res) => {
+    request(url, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+        //    console.log(body);
+        var list = JSON.parse(body).list;
+        var weather = [];
+            for(let i=0;i<list.length;i++){
+                weather.push(
+                    {"temp":list[i].temp.day,
+                    "tempMin":list[i].temp.min,
+                    "tempMax":list[i].temp.max,
+                    "humidity":list[i].humidity,
+                    "date":list[i].dt,
+                    "icon":list[i].weather[0].icon,
+                    "description":list[i].weather[0].description
+                });
+            }
+            //console.log(weather);
+            res.json(weather)
+        }
+ 
+    })
+
 })
+
+
 
 app.listen(8080, () => {
 	console.log('Server Started on http://localhost:8011');
@@ -44,3 +54,12 @@ app.listen(8080, () => {
 //http://api.openweathermap.org/data/2.5/forecast?q=Toronto,ca&APPID=517985c009ccca68942debb4afe4feb2
 
 //try Accessing sunrise and sunset time 
+
+// {"temp":list[i].main.temp,
+//                     "tempMin":list[i].main.temp_min,
+//                     "tempMax":list[i].main.temp_max,
+//                     "humidity":list[i].main.humidity,
+//                     "date":list[i].dt_txt,
+//                     "icon":list[i].weather[0].icon,
+//                     "description":list[i].weather[0].description
+//                 });
--- frontEnd/src/App.js
+++ frontEnd/src/App.js
@@ -1,10 +1,35 @@
 import React, { Component } from 'react';
 import Header from './Header'
-
+import axios from 'axios'
 class App extends Component {
+constructor(){
+    super();
+    this.state={
+        data:'',
+        loading:true
+    }
+}
+
+componentWillMount(){
+    axios.get('http://localhost:8080/').then((res)=>{
+        //console.log(res);
+        this.setState({
+            data:res.data,
+            loading:false
+        })
+    })
+}
   render() {
+      if(this.state.loading){
+          return<div>Loading........</div>
+      }else{
+            let date =this.state.data[0].date;
+            let newDate = Date(date);
+            console.log(newDate.substring(0,15));
+      }
     return (
       <Header />
+
     );
   }
 }
